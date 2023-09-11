8b44c0bd4709c878cdd0be10a4addb6dd6c167a9
Keisn1 <kay.freyer@icloud.com>
2023-09-11 23:32:25 +0200

    add ubuntu config

---
+++ .ubuntu_setup.sh
@@ -0,0 +1,31 @@
+# a comment
+sudo apt update && sudo apt upgrade
+
+sudo apt install git-all
+
+git config --global user.name "Keisn1"
+git config --global user.email kay.freyer@icloud.com
+
+git clone https://github.com/Keisn1/.dotfiles.git
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/KayProAsusUbuntu_rsa
+
+sudo apt install ubuntu-restricted-extras
+
+sudo apt install gnome-tweaks dconf-editor
+
+dconf load / < dconf-settings.ini
+
+# dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < ~/.dotfiles/terminal/profiles/ubuntu1.dconf
+
+FONT_VERSION=$(curl -s "https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -sSLo jetbrains-mono.zip https://download.jetbrains.com/fonts/JetBrainsMono-$FONT_VERSION.zip
+unzip -qq jetbrains-mono.zip -d jetbrains-mono
+sudo mkdir ~/.local/share/fonts/jetbrains-mono-nerd/
+sudo mv jetbrains-mono/fonts/ttf/*.ttf ~/.local/share/fonts/jetbrains-mono-nerd/
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+pyenv install $(pyenv install --list | grep -v - | grep -v b | grep 3.11 | tail -n 1)
+
+ pip install ~/.dotfiles/python/requirements-dev.txt
