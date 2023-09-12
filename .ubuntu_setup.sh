c98e945aeb76f36a0be439f3f2e39804f690a803
Keisn1 <kay.freyer@icloud.com>
2023-09-12 13:20:41 +0200

    Correct the dir for installing oh my zsh plugins

--- .ubuntu_setup.sh
+++ .ubuntu_setup.sh
@@ -37,8 +37,8 @@ doom install
 sudo apt install git-core zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo apt install fonts-powerline
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 sudo apt-get -y install stow
 
