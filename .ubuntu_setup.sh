73822a1fdd8ae81d7e4fa1b062f77fb313273c7d
Keisn1 <kay.freyer@icloud.com>
2023-09-12 16:10:27 +0200

    Add Python requirements

--- .ubuntu_setup.sh
+++ .ubuntu_setup.sh
@@ -75,4 +75,23 @@ sudo apt install npm
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 pyenv install $(pyenv install --list | grep -v - | grep -v b | grep 3.11 | tail -n 1)
 
+sudo apt install \
+    build-essential \
+    curl \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    llvm \
+    make \
+    tk-dev \
+    wget \
+    xz-utils \
+    zlib1g-dev
+
 pip install ~/.dotfiles/python/requirements-dev.txt
