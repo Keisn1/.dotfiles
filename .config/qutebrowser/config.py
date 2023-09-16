2c24743df1a15e1f8c4a293cecfbd34ee6d538ad
Keisn1 <kay.freyer@icloud.com>
2023-09-16 21:28:31 +0200

    Add qtile and alacritty

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -200,4 +200,4 @@ config.bind("<Ctrl+j>", "completion-item-focus --history next", mode="command")
 config.bind("<Ctrl+k>", "completion-item-focus --history prev", mode="command")
 
 config.bind("M", "hint links spawn mpv {hint-url}")
-config.bind("Z", "hint links spawn st -e youtube-dl {hint-url}")
+config.bind("Z", "hint links spawn yt-dlp {hint-url}")
